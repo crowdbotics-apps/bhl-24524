7cd87d2cf9544b2f8fc2bda2abdab472d69f676f
zd15183p <47407424+zd15183p@users.noreply.github.com>
2021-02-14 11:36:21 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,64 +1,60 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {Provider as ReduxProvider} from 'react-redux';
+/* eslint-disable prettier/prettier */
+import 'react-native-gesture-handler';
+import React, {useEffect, useState} from 'react';
+/* import SplashScreen from 'react-native-splash-screen'; */
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
-import SplashScreen from './src/features/SplashScreen';
-import {store} from './src/store';
-import NavigatorProvider from './src/navigator/mainNavigator';
-import {setupHttpConfig} from './src/utils/http';
-import * as NavigationService from './src/navigator/NavigationService';
+//import Screens
+import HomePage from './src/screens/HomePage';
+import AlertsPageNavigator from './src/screens/AlertsPage';
+import LocationPage from './src/screens/LocationPage';
+import LinksPage from './src/screens/LinksPage';
+/* import MapsPage from './src/screens/MapsPage';
+ */
+import {DrawerContent} from './src/DrawerContent';
 
-export default class App extends React.Component {
-  state = {
-    isLoaded: false,
-  };
+// import Onboarding from './src/screens/Onboarding/Onboarding';
 
-  async componentWillMount() {
-    /**
-     * add any aditional app config here,
-     * don't use blocking requests here like HTTP requests since they block UI feedback
-     * create HTTP requests and other blocking requests using redux saga
-     */
-    await this.loadAssets();
-    setupHttpConfig();
-  }
+const Drawer = createDrawerNavigator();
+const MainStack = createStackNavigator();
 
-  componentDidMount() {
-    /**
-     * Read above commments above adding async requests here
-     */
-    NavigationService.setNavigator(this.navigator);
-  }
+function MainPageNavigator() {
+  return (
+    <MainStack.Navigator
+      initialRouteName="Home"
+      headerMode="true"
+      // screenOptions={{...TransitionPresets.SlideFromRightIOS}}
+    >
 
-  loadAssets = async () => {
-    // add any loading assets here
-    this.setState({isLoaded: true});
-  };
-
-  renderLoading = () => (
-    <View style={[styles.flex]}>
-      <Text>Loading</Text>
-    </View>
+      {/* <MainStack.Screen name="Onboarding" component={Onboarding} /> */}
+      <MainStack.Screen name="Home" component={HomePage} />
+      <MainStack.Screen name="Alerts" component={AlertsPageNavigator} />
+      <MainStack.Screen name="Location" component={LocationPage} />
+      <MainStack.Screen name="Links" component={LinksPage} />
+      {/*       <MainStack.Screen name="Maps" component={MapsPage} />
+       */}
+    </MainStack.Navigator>
   );
-
-  renderApp = () => (
-    <ReduxProvider store={store}>
-      <NavigatorProvider
-        style={styles.flex}
-        ref={(nav) => {
-          this.navigator = nav;
-        }}>
-        <View style={[styles.flex]}>
-          <SplashScreen />
-        </View>
-      </NavigatorProvider>
-    </ReduxProvider>
-  );
-
-  render = () =>
-    this.state.isLoaded ? this.renderApp() : this.renderLoading();
 }
 
-const styles = StyleSheet.create({
-  flex: {flex: 1},
-});
+export default function App() {
+  const [initRender, setInitRender] = useState(true);
+
+  useEffect(() => {
+    /* SplashScreen.hide(); */
+    setInitRender(false);
+  }, [initRender]);
+
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName="MainPageNavigator"
+        drawerContent={(props) => <DrawerContent {...props} />}
+        drawerStyle={{width: initRender ? null : '70%'}}>
+        <Drawer.Screen name="MainPageNavigator" component={MainPageNavigator} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
